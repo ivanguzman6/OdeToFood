4855f932a0d5e737c028360114ad08ab127f2b0a
Ivan Guzman Brito <ivanguzman6@hotmail.com>
2020-04-05 14:47:23 -0400

    Add project files.

---
+++ OdeToFood.Data/Models/Restaurant.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OdeToFood.Data.Models
+{
+    public class Restaurant
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
